e93458cc18adfe0f5fae2b8a1b49e5cdf6100be4
Min Kim <mk4555@nyu.edu>
2017-11-14 20:50:34 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,3 +6,8 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
 end
 #projects: create_project_hash.css("li.project.grid_4").first
+#project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+project = create_project_hash
+binding.pry
