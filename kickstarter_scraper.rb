3fb02e8245eb55c097465f9ad3ba5a29c15a13ef
Min Kim <mk4555@nyu.edu>
2017-11-14 20:53:18 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,7 @@ require('nokogiri')
 require('pry')
 def create_project_hash
   # write your code here
-  html = Files.read('fixtures/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 end
 #projects: create_project_hash.css("li.project.grid_4").first
