e33cfbbe862e13370432db12ea1afbb1d7ad154d
Min Kim <mk4555@nyu.edu>
2017-11-14 21:32:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -22,5 +22,3 @@ end
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
 # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-project = create_project_hash
-binding.pry
